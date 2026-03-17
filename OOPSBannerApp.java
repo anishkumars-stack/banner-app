6c00c73b72a5500400f6f035b84c14a720099d92
Anish Kumar <anishkumarak2007@gmail.com>
2026-03-17 17:20:17 +0530

    Added UC8 - HashMap based banner rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,58 +1,73 @@
- public class OOPSBannerApp {
+import java.util.HashMap;
+import java.util.Map;
+
+public class OopsBannerUC8 {
+
+    // Map to store character patterns
+    static Map<Character, String[]> patternMap = new HashMap<>();
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", getO()[0], getO()[0], getP()[0], getS()[0]),
-            String.join(" ", getO()[1], getO()[1], getP()[1], getS()[1]),
-            String.join(" ", getO()[2], getO()[2], getP()[2], getS()[2]),
-            String.join(" ", getO()[3], getO()[3], getP()[3], getS()[3]),
-            String.join(" ", getO()[4], getO()[4], getP()[4], getS()[4]),
-            String.join(" ", getO()[5], getO()[5], getP()[5], getS()[5]),
-            String.join(" ", getO()[6], getO()[6], getP()[6], getS()[6])
-        };
+        initializePatterns();
 
-        for (String line : banner) {
+        String word = "OOPS";
+
+        renderBanner(word);
+    }
+
+    // Function to store patterns
+    public static void initializePatterns() {
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+    }
+
+    // Function to render banner
+    public static void renderBanner(String word) {
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("  ");
+                }
+            }
+
             System.out.println(line);
         }
-    }
-
-    // Pattern for letter O
-    public static String[] getO() {
-        return new String[]{
-            " ***** ",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            "*     *",
-            " ***** "
-        };
-    }
-
-    // Pattern for letter P
-    public static String[] getP() {
-        return new String[]{
-            " ***** ",
-            "*     *",
-            "*     *",
-            " ***** ",
-            "*      ",
-            "*      ",
-            "*      "
-        };
-    }
-
-    // Pattern for letter S
-    public static String[] getS() {
-        return new String[]{
-            " ***** ",
-            "*      ",
-            "*      ",
-            " ***** ",
-            "      *",
-            "      *",
-            " ***** "
-        };
     }
 }
