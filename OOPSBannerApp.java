4254777f253d3b759146943fb1420042bfed6b21
Anish Kumar <anishkumarak2007@gmail.com>
2026-03-17 17:12:54 +0530

    UC6: Refactor banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,58 @@
-public class OOPSBannerApp {
+ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize the banner array in one step
         String[] banner = {
-            String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join(" ", "*     *", "*     *", "*     *", "*     "),
-            String.join(" ", "*     *", "*     *", "*     *", "*     "),
-            String.join(" ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join(" ", "*     *", "*     *", "*      ", "     *"),
-            String.join(" ", "*     *", "*     *", "*      ", "     *"),
-            String.join(" ", " ***** ", " ***** ", "*      ", " ***** ")
+            String.join(" ", getO()[0], getO()[0], getP()[0], getS()[0]),
+            String.join(" ", getO()[1], getO()[1], getP()[1], getS()[1]),
+            String.join(" ", getO()[2], getO()[2], getP()[2], getS()[2]),
+            String.join(" ", getO()[3], getO()[3], getP()[3], getS()[3]),
+            String.join(" ", getO()[4], getO()[4], getP()[4], getS()[4]),
+            String.join(" ", getO()[5], getO()[5], getP()[5], getS()[5]),
+            String.join(" ", getO()[6], getO()[6], getP()[6], getS()[6])
         };
 
-        // Print banner using enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Pattern for letter O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Pattern for letter P
+    public static String[] getP() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Pattern for letter S
+    public static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
